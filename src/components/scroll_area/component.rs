64c3ee893532dcd869439421b16184bff19a480f
Moritz Brödel <moritz.broedel@gmail.com>
2026-02-18 23:29:12 +0100

    Remove dx-components-theme ref GC-39

--- src/components/scroll_area/component.rs
+++ src/components/scroll_area/component.rs
@@ -3,8 +3,5 @@ use dioxus_primitives::scroll_area::{self, ScrollAreaProps};
 
 #[component]
 pub fn ScrollArea(props: ScrollAreaProps) -> Element {
-    rsx! {
-        document::Link { rel: "stylesheet", href: asset!("./style.css") }
-        { scroll_area::ScrollArea(props) }
-    }
+    scroll_area::ScrollArea(props)
 }
